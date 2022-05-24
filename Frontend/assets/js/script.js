dfe0b128e359c7d6826c32e064313338a6a6dc56
JonCourbon <joncourbon@gmail.com>
2022-05-24 14:28:59 +0200

    ajout script js dans public

--- Frontend/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-  $(".savoirplus").click(function () { 
-    var id = $(this).data("id");
-    $.get("API/dist/getActeur.php",{ id: id}, function(data, status){
-      alert("Infos: "+data);
-    });
-  });
-});
